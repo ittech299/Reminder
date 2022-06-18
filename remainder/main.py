54327bc38858eb2d9ace93724c6444149cf93f94
itsc <91063455+ittech299@users.noreply.github.com>
2022-06-18 21:30:10 +0530

    Update main.py

--- remainder/main.py
+++ remainder/main.py
@@ -2,7 +2,9 @@ import pandas as pd
 from plyer import notification
 import time
 
-
+#remainder is a File name
+#Enter Your remainder.xlsx file location 
+#Don't Forget to put the .xlsx in the end of the file name in your path
 data = pd.read_excel (r'remainder.xlsx')
 
 
@@ -10,6 +12,7 @@ data = pd.read_excel (r'remainder.xlsx')
 df = pd.DataFrame(data,columns=['Time','Task'])    
 istime = False
 while True:
+    #This is For The time
     t = time.localtime()
     current_time = time.strftime("%I:%M:%S %p", t).lstrip('0')
 
@@ -18,42 +21,17 @@ while True:
                 condition = df["Time"] == current_time
                 li = index[condition]
                 lst = li.tolist()
+                #This is For the Notification
                 notification.notify(title = str(df.values[lst,1]).replace("['", "").replace("']", "").capitalize()+" Time",message = 'Your Task',timeout = 15)
                 istime = False
                 time.sleep(15)
     else:
         istime = True
         time.sleep(1)
-        
-
-    
-                
+  
             
     
         
 
 
-  
-
-  # index = df.index
-  # condition = df["Time"] == current_time
-  # li = index[condition]
-  # lst = li.tolist()
-  # print(df.values[lst,1], current_time)
-    
-        
-
-
-
-
-
-#First  Method
-#*******
-#if "12:39:40" in df.values:
-#        
-#        #break
-#else:
-#        print(current_time)
-#
-
-#print(df.values[0,1])
+ 
